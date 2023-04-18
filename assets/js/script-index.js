7609d2a5f00c6e9175f789c4b9156d422effe63d
Edward Wells <edward.wells87@gmail.com>
2023-04-18 11:30:59 -0500

    function for search works

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -16,22 +16,45 @@ const options = {
 //}
 var movieClicker = document.getElementById('button1')
 const userInput = document.getElementById('search');
+const selector = document.getElementById('searchType')
+
+
+
 
 function theClick() {
   event.preventDefault();
   console.log('thebuttonwasclicked')
   const inputValue = userInput.value;
-  console.log(userInput.value)
-  myMovieSearchArry = ['https://moviesdatabase.p.rapidapi.com/titles/search/keyword/', inputValue, '?limit=10']
-  movieSearchString = myMovieSearchArry.join('')
-  console.log(myMovieSearchArry)
-  console.log(movieSearchString)
-  fetch(movieSearchString, options)
-    .then(response => response.json())
-    .then(response => {
-      localStorage.setItem('movieSearchResults', JSON.stringify(response));
-      console.log(response);
-    })
-    .catch(err => console.error(err));
-  alert('Stored!')
-};
+  if (searchType === "movies") {
+    console.log("Searching for movies...");
+    console.log(userInput.value)
+    myMovieSearchArry = ['https://moviesdatabase.p.rapidapi.com/titles/search/keyword/', inputValue, '?limit=10']
+    movieSearchString = myMovieSearchArry.join('')
+    console.log(myMovieSearchArry)
+    console.log(movieSearchString)
+    fetch(movieSearchString, options)
+      .then(response => response.json())
+      .then(response => {
+        localStorage.setItem('movieSearchResults', JSON.stringify(response));
+        console.log(response);
+      })
+      .catch(err => console.error(err));
+    alert('Stored!')
+  } else {
+
+    const options1 = {
+      method: 'GET',
+      headers: {
+        Type: 'get-shows-by-title',
+        'X-RapidAPI-Key': 'b4653a9300msh9edce1d2439a532p1ecf5ejsn6f47fc20c7fd',
+        'X-RapidAPI-Host': 'movies-tv-shows-database.p.rapidapi.com'
+      }
+    };
+    
+    fetch(`https://movies-tv-shows-database.p.rapidapi.com/?title=${inputValue}`, options1)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+  //need a function to select which option was selected 
+  }
+}
