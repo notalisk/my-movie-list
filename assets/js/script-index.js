7bda5537c0e290c7b63b0d677dc45f1c76c5ebed
Edward Wells <edward.wells87@gmail.com>
2023-04-18 10:58:56 -0500

    fixed the button

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -1,5 +1,3 @@
-
-
 const options = {
   method: 'GET',
   headers: {
@@ -19,15 +17,12 @@ const options = {
 var movieClicker = document.getElementById('button1')
 const userInput = document.getElementById('search');
 
-movieClicker.addEventListener('click', theButtonWasClicked);
-function theButtonWasClicked() {
+function theClick() {
   event.preventDefault();
   console.log('thebuttonwasclicked')
   const inputValue = userInput.value;
   console.log(userInput.value)
-
-
-  myMovieSearchArry = ['https://moviesdatabase.p.rapidapi.com/titles/search/title/', inputValue, '?limit=4']
+  myMovieSearchArry = ['https://moviesdatabase.p.rapidapi.com/titles/search/keyword/', inputValue, '?limit=10']
   movieSearchString = myMovieSearchArry.join('')
   console.log(myMovieSearchArry)
   console.log(movieSearchString)
@@ -38,72 +33,5 @@ function theButtonWasClicked() {
       console.log(response);
     })
     .catch(err => console.error(err));
-
   alert('Stored!')
 };
-/// 
-//function that pulls out the array of movie data from local storage 
-//lets called the stored arry "seenItStorageArry"
-//lets call the array "MyFavMoviesArry" when it comes out
-
-
-
-//////////////////////// day 1 changes
-//let gottaTvDataArry = JSON.parse('gottaSeeTvStoreArry')
-//gottaTvDataArry.forEach(function(obj, index) {
-// if (index < gottaSeeTvElmArry.length) {
-//  gottaSeeTvElmArry[index].value = obj.title;
-// }
-//})
-
-
-let movieSearchResultsJSON = localStorage.getItem('movieSearchResults');
-
-let movieResultsArray = JSON.parse(movieSearchResultsJSON);
-
-console.log(movieResultsArray);
-//array of title containers
-let ResultTitle1 = document.getElementById('titleSec1')
-let ResultTitle2 = document.getElementById('titleSec2')
-let ResultTitle3 = document.getElementById('titleSec3')
-let ResultTitle4 = document.getElementById('titleSec4')
-let ResultTitleArry = [ResultTitle1, ResultTitle2, ResultTitle3, ResultTitle4]
-
-//target the image containers. (make an array of image containers and then eventually populate them with a loop) 
-let resultsImg1 = document.getElementById('imgsection1')
-let resultsImg2 = document.getElementById('imgsection2')
-let resultsImg3 = document.getElementById('imgsection3')
-let resultsImg4 = document.getElementById('imgsection4')
-let resultImgArry = [resultsImg1, resultsImg2, resultsImg3, resultsImg4]
-
-//call the image from the results array 
-//let Img1FrmResults = movieResultsArray.results[0].primaryImage
-//console.log(Img1FrmResults.url)
-//resultsImg1.src = Img1FrmResults.url;
-
-//let title1 = movieResultsArray.results[0].titleText.text;
-//let ResultTitle1 = document.getElementById('titleSec1')
-//console.log(title1);
-//ResultTitle1.textContent = title1
-
-//make an array of image result elements
-console.log(movieResultsArray.results[0].primaryImage.url)
-
-///loop for img results population
-for (let i = 0; i < movieResultsArray.results.length; i++) {
-  if (movieResultsArray.results[i].primaryImage !== null) {
-    
-    resultImgArry[i].src = movieResultsArray.results[i].primaryImage.url
-  } else {
-    resultImgArry[i].src = './assets/defaultMissing.png';
-  }
-}
-// loop for movie titles pop
-for (let j = 0; j < ResultTitleArry.length; j++) {
-  if (movieResultsArray.results[j].titleText.text !== null) {
-    ResultTitleArry[j].textData = movieResultsArray.results[j].titleText.text;
-  } else {
-    ResultTitleArry[j].textData = "Sorry No Result Listed"
-  }
-}
-
